0ea9f1106f5983987374c5dbc661108ba21b8919
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2022-03-10 11:17:28 +0100

    sidebar component

---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,32 @@
+import './style.css';
+
+import selfcare from './assets/selfcare.svg';
+import swim from './assets/swim.svg';
+import bike from './assets/bike.svg';
+import weight from './assets/weight.svg';
+
+function SideBar() {
+  return (
+    <div id="side_bar">
+      <ul id="side_nav">
+        <li>
+          <img src={selfcare} alt="" />
+        </li>
+        <li>
+          <img src={swim} alt="" />
+        </li>
+        <li>
+          <img src={bike} alt="" />
+        </li>
+        <li>
+          <img src={weight} alt="" />
+        </li>
+      </ul>
+      <div id="side_copyright">
+        <p>Copiryght, SportSee 2020</p>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
