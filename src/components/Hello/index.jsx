dd86ab5462a7373d8eb435a7e2e63203482da091
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2022-03-10 13:39:25 +0100

    hello componant styled

--- src/components/Hello/index.jsx
+++ src/components/Hello/index.jsx
@@ -1,7 +1,16 @@
 import './style.css';
 
 function Hello() {
-  return <div id="hello"></div>;
+  return (
+    <div id="hello">
+      <p id="hello_name">
+        Bonjour <span>James</span>
+      </p>
+      <p id="hello_congrats">
+        Félicitation ! Vous avez explosé vos objectifs hier 👏
+      </p>
+    </div>
+  );
 }
 
 export default Hello;
