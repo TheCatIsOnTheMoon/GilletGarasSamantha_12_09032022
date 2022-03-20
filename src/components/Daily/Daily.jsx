c02f24f2f0e8d5b895940c45d0eefaba4dd370ab
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2022-03-20 14:26:45 +0100

    install and tweak Jsdoc

--- src/components/Daily/Daily.jsx
+++ src/components/Daily/Daily.jsx
@@ -3,6 +3,14 @@ import { ACTIVITY_DATA_URL } from '../../constants';
 import { useFetch } from '../../Hooks/FetchData/FetchData';
 import DailyChart from '../DailyChart/DailyChart';
 
+/**
+ * It takes the data from the API and formats it into a format that the chart can use
+ * @component
+ * @example
+ * return (
+ *   <Daily />
+ * )
+ */
 function Daily() {
   const { data, isLoading, error } = useFetch(ACTIVITY_DATA_URL);
   const ACTIVITY_DATA = data;
@@ -16,11 +24,16 @@ function Daily() {
   // ACTIVITY_DATA && console.log('ACTIVITY_DATA 3 :', ACTIVITY_DATA); //OK
   const activityData = ACTIVITY_DATA.data.sessions;
 
+  /**
+   * Given a date string, return a new date string with the month and day swapped
+   * @returns The date without the leading zeros.
+   */
   function formatDate(date) {
     let newDate = date.substring(8);
     return newDate.replace(/^0+/, '');
   }
 
+  /* Creating a new array with the same structure as the original array, but with the day value formatted. */
   const newData = activityData.map((item) => {
     return {
       day: formatDate(item.day),
