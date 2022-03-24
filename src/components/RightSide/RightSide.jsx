92d7d7f568824a1776dde5abc1c01b6f567e1d18
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2022-03-24 17:17:07 +0100

    refacto RightSide components

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -4,6 +4,11 @@ import { useFetch } from '../../Hooks/FetchData/FetchData';
 import RightSideDetails from '../RightSideDetails/RightSideDetails';
 import Loader from '../Loader/Loader';
 
+import energy from './assets/energy.svg';
+import chicken from './assets/chicken.svg';
+import apple from './assets/apple.svg';
+import cheeseburger from './assets/cheeseburger.svg';
+
 /**
  * It takes the main data from the API and formats it into a format that the `RightSideDetails` componant can use
  * @component
@@ -21,13 +26,45 @@ function RightSide() {
 
   const KEY_DATA = data.data.keyData;
 
+  const NEW_KEY_DATA = [
+    {
+      name: 'calories',
+      value: KEY_DATA.calorieCount,
+      unit: 'Kcal',
+      icon: energy,
+    },
+    {
+      name: 'proteines',
+      value: KEY_DATA.proteinCount,
+      unit: 'g',
+      icon: chicken,
+    },
+    {
+      name: 'glucides',
+      value: KEY_DATA.carbohydrateCount,
+      unit: 'g',
+      icon: apple,
+    },
+    {
+      name: 'lipides',
+      value: KEY_DATA.lipidCount,
+      unit: 'g',
+      icon: cheeseburger,
+    },
+  ];
+
   return (
-    <RightSideDetails
-      calories={KEY_DATA.calorieCount}
-      proteines={KEY_DATA.proteinCount}
-      glucides={KEY_DATA.carbohydrateCount}
-      lipides={KEY_DATA.lipidCount}
-    />
+    <div>
+      {NEW_KEY_DATA.map((e, index) => (
+        <RightSideDetails
+          key={`${e.name}-${index}`}
+          name={e.name}
+          value={e.value}
+          unit={e.unit}
+          icon={e.icon}
+        />
+      ))}
+    </div>
   );
 }
 
--- src/components/RightSideDetails/RightSideDetails.jsx
+++ src/components/RightSideDetails/RightSideDetails.jsx
@@ -1,70 +1,36 @@
 import './style.css';
 import PropTypes from 'prop-types';
-import energy from './assets/energy.svg';
-import chicken from './assets/chicken.svg';
-import apple from './assets/apple.svg';
-import cheeseburger from './assets/cheeseburger.svg';
 
 /**
  * React component that display a recap of the daily calories, proteines, glucides and lipides of the user.
  * @component
- * @param  {number} props User calories
- * @param  {number} props User proteines
- * @param  {number} props User glucides
- * @param  {number} props User lipides
+ * @param  {string} props type of the data
+ * @param  {number} props value of the data
+ * @param  {string} props unit of the data
+ * @param  {string} props icon path of the data
  *
  */
-function RightSideDetails({ calories, proteines, glucides, lipides }) {
+function RightSideDetails({ name, value, unit, icon }) {
   return (
-    <div id="rightDetails">
-      <div className="rightDetails_box" id="box_calories">
-        <div className="icon_box">
-          <img src={energy} alt="energy icon" className="icon" />
-        </div>
-        <div className="info">
-          <p className="infos_quantity">{calories}kCal</p>
-          <p className="infos_label">Calories</p>
-        </div>
+    <div className="rightDetails_box" id={`box_${name}`}>
+      <div className="icon_box">
+        <img src={icon} alt={`${name} icon`} className="icon" />
       </div>
-
-      <div className="rightDetails_box" id="box_proteines">
-        <div className="icon_box">
-          <img src={chicken} alt="chicken icon" className="icon" />
-        </div>
-        <div className="info">
-          <p className="infos_quantity">{proteines}g</p>
-          <p className="infos_label">Proteines</p>
-        </div>
-      </div>
-
-      <div className="rightDetails_box" id="box_glucides">
-        <div className="icon_box">
-          <img src={apple} alt="apple icon" className="icon" />
-        </div>
-        <div className="info">
-          <p className="infos_quantity">{glucides}g</p>
-          <p className="infos_label">Glucides</p>
-        </div>
-      </div>
-
-      <div className="rightDetails_box" id="box_lipides">
-        <div className="icon_box">
-          <img src={cheeseburger} alt="cheeseburger icon" className="icon" />
-        </div>
-        <div className="info">
-          <p className="infos_quantity">{lipides}g</p>
-          <p className="infos_label">Lipides</p>
-        </div>
+      <div className="info">
+        <p className="infos_quantity">
+          {value} {unit}
+        </p>
+        <p className="infos_label">{name}</p>
       </div>
     </div>
   );
 }
 
 RightSideDetails.propTypes = {
-  calories: PropTypes.number,
-  proteines: PropTypes.number,
-  glucides: PropTypes.number,
-  lipides: PropTypes.number,
+  name: PropTypes.string,
+  value: PropTypes.number,
+  unit: PropTypes.string,
+  icon: PropTypes.string,
 };
 
 export default RightSideDetails;
