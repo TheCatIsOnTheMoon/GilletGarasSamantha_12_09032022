5b822b07f6b63b203cde276af6cc8469d2f6c262
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2022-03-25 20:25:52 +0100

    redirect to error page if wrong user id

--- src/Hooks/GetUserID/GetUserID.jsx
+++ src/Hooks/GetUserID/GetUserID.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 /**
  * Given the URL, return the user ID from the URL
@@ -6,7 +7,14 @@ import { useLocation } from 'react-router-dom';
  */
 function GetUserID() {
   const sampleLocation = useLocation();
+  const navigate = useNavigate();
   const userID = sampleLocation.pathname.substring(11);
+
+  //for demo purpose only
+  if ((userID !== '12') & (userID !== '18')) {
+    navigate('*');
+  }
+
   return userID;
 }
 
